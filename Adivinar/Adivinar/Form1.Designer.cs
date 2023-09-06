5e593fff8fabfb56e09608c3e86f656d322588f1
Cristian <cristianjosuer9@gmail.com>
2023-09-06 15:53:42 -0600

    Agregado Adivinanza trabajando

---
+++ Adivinar/Adivinar/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace Adivinar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pcb1 = new System.Windows.Forms.PictureBox();
+            this.pcb2 = new System.Windows.Forms.PictureBox();
+            this.pcb3 = new System.Windows.Forms.PictureBox();
+            this.pcb4 = new System.Windows.Forms.PictureBox();
+            this.pcb5 = new System.Windows.Forms.PictureBox();
+            this.pcb6 = new System.Windows.Forms.PictureBox();
+            this.pcb7 = new System.Windows.Forms.PictureBox();
+            this.pcb8 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcb1
+            // 
+            this.pcb1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pcb1.Location = new System.Drawing.Point(188, 125);
+            this.pcb1.Name = "pcb1";
+            this.pcb1.Size = new System.Drawing.Size(190, 210);
+            this.pcb1.TabIndex = 0;
+            this.pcb1.TabStop = false;
+            this.pcb1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pcb_Click);
+            this.pcb1.MouseEnter += new System.EventHandler(this.pcb_MouseEnter);
+            this.pcb1.MouseLeave += new System.EventHandler(this.pcb_MouseLeave);
+            // 
+            // pcb2
+            // 
+            this.pcb2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pcb2.Location = new System.Drawing.Point(399, 125);
+            this.pcb2.Name = "pcb2";
+            this.pcb2.Size = new System.Drawing.Size(190, 210);
+            this.pcb2.TabIndex = 1;
+            this.pcb2.TabStop = false;
+            this.pcb2.MouseEnter += new System.EventHandler(this.pcb_MouseEnter);
+            this.pcb2.MouseLeave += new System.EventHandler(this.pcb_MouseLeave);
+            // 
+            // pcb3
+            // 
+            this.pcb3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pcb3.Location = new System.Drawing.Point(609, 125);
+            this.pcb3.Name = "pcb3";
+            this.pcb3.Size = new System.Drawing.Size(190, 210);
+            this.pcb3.TabIndex = 3;
+            this.pcb3.TabStop = false;
+            // 
+            // pcb4
+            // 
+            this.pcb4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pcb4.Location = new System.Drawing.Point(821, 125);
+            this.pcb4.Name = "pcb4";
+            this.pcb4.Size = new System.Drawing.Size(190, 210);
+            this.pcb4.TabIndex = 2;
+            this.pcb4.TabStop = false;
+            // 
+            // pcb5
+            // 
+            this.pcb5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pcb5.Location = new System.Drawing.Point(399, 366);
+            this.pcb5.Name = "pcb5";
+            this.pcb5.Size = new System.Drawing.Size(190, 210);
+            this.pcb5.TabIndex = 5;
+            this.pcb5.TabStop = false;
+            // 
+            // pcb6
+            // 
+            this.pcb6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pcb6.Location = new System.Drawing.Point(188, 366);
+            this.pcb6.Name = "pcb6";
+            this.pcb6.Size = new System.Drawing.Size(190, 210);
+            this.pcb6.TabIndex = 4;
+            this.pcb6.TabStop = false;
+            // 
+            // pcb7
+            // 
+            this.pcb7.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pcb7.Location = new System.Drawing.Point(609, 366);
+            this.pcb7.Name = "pcb7";
+            this.pcb7.Size = new System.Drawing.Size(190, 210);
+            this.pcb7.TabIndex = 7;
+            this.pcb7.TabStop = false;
+            // 
+            // pcb8
+            // 
+            this.pcb8.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pcb8.Location = new System.Drawing.Point(821, 366);
+            this.pcb8.Name = "pcb8";
+            this.pcb8.Size = new System.Drawing.Size(190, 210);
+            this.pcb8.TabIndex = 6;
+            this.pcb8.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(1273, 663);
+            this.Controls.Add(this.pcb7);
+            this.Controls.Add(this.pcb8);
+            this.Controls.Add(this.pcb5);
+            this.Controls.Add(this.pcb6);
+            this.Controls.Add(this.pcb3);
+            this.Controls.Add(this.pcb4);
+            this.Controls.Add(this.pcb2);
+            this.Controls.Add(this.pcb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb8)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcb1;
+        private System.Windows.Forms.PictureBox pcb2;
+        private System.Windows.Forms.PictureBox pcb3;
+        private System.Windows.Forms.PictureBox pcb4;
+        private System.Windows.Forms.PictureBox pcb5;
+        private System.Windows.Forms.PictureBox pcb6;
+        private System.Windows.Forms.PictureBox pcb7;
+        private System.Windows.Forms.PictureBox pcb8;
+    }
+}
+
---
+++ Adivinar/Adivinar/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adivinar
+{
+    public partial class Form1 : Form
+    {
+        private Color defaulColor = Color.Red;
+
+        private int clicks = 0;
+
+        private Image[] listaImagenes = new Image[]{
+        Image.FromFile("Imagenes/zorro.jpg"),
+        Image.FromFile("Imagenes/dinosaurio.jpg"),
+        Image.FromFile("Imagenes/gato.jpg"),
+        Image.FromFile("Imagenes/unicornio.jpg"),
+        // Agrega más imágenes según tus necesidades
+        };
+
+        private Dictionary<int, bool> Calculados = new Dictionary<int, bool>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c is PictureBox)
+                    // ((PictureBox)c).Visible = true;
+                ((PictureBox)c).BackColor = Color.Red;
+                ((PictureBox)c).SizeMode = PictureBoxSizeMode.StretchImage;
+
+                PictureBox pictureBox = (PictureBox)c;
+
+                // Asigna el manejador de eventos MouseEnter
+                pictureBox.MouseEnter += new EventHandler(pcb_MouseEnter);
+
+                // Asigna el manejador de eventos MouseLeave
+                pictureBox.MouseLeave += new EventHandler(pcb_MouseLeave);
+
+                pictureBox.MouseClick += new MouseEventHandler(pcb_Click);
+            }
+
+        }
+
+        private void pcb_MouseEnter(object sender, EventArgs e)
+        {
+            ((PictureBox)sender).BackColor = Color.Orange;
+        }
+
+        private void pcb_MouseLeave(object sender, EventArgs e)
+        {
+            ((PictureBox)sender).BackColor = this.defaulColor;
+
+        }
+
+        private void pcb_Click(object sender, MouseEventArgs e)
+        {
+            Image imagen = Image.FromFile("Imagenes/zorro.jpg");
+
+            Image imagen2 = Image.FromFile("Imagenes/zorro.jpg");
+
+            ((PictureBox)sender).Image = imagen;
+
+        }
+
+
+    }
+}
