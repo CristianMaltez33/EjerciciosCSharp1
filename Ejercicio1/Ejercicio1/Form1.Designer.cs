78a49898d0428ccbd53ef23b2072968b6c3de73b
Cristian <cristianjosuer9@gmail.com>
2023-09-06 10:40:31 -0600

    Primer ejercicio sin terminar

---
+++ Ejercicio1/Ejercicio1/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace Ejercicio1
+{
+    partial class ejercicio1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumeros = new System.Windows.Forms.Label();
+            this.txtNumeros = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblNumeros
+            // 
+            this.lblNumeros.AutoSize = true;
+            this.lblNumeros.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumeros.Location = new System.Drawing.Point(146, 88);
+            this.lblNumeros.Name = "lblNumeros";
+            this.lblNumeros.Size = new System.Drawing.Size(126, 29);
+            this.lblNumeros.TabIndex = 0;
+            this.lblNumeros.Text = "Números:";
+            // 
+            // txtNumeros
+            // 
+            this.txtNumeros.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumeros.Location = new System.Drawing.Point(355, 90);
+            this.txtNumeros.Name = "txtNumeros";
+            this.txtNumeros.Size = new System.Drawing.Size(273, 30);
+            this.txtNumeros.TabIndex = 1;
+            this.txtNumeros.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtNumeros_KeyDown);
+            this.txtNumeros.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumeros_KeyPress);
+            this.txtNumeros.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtNumeros_KeyUp);
+            // 
+            // ejercicio1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNumeros);
+            this.Controls.Add(this.lblNumeros);
+            this.Name = "ejercicio1";
+            this.Text = "ejercicio1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumeros;
+        private System.Windows.Forms.TextBox txtNumeros;
+    }
+}
+
---
+++ Ejercicio1/Ejercicio1/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class ejercicio1 : Form
+    {
+        private int dotCounted = 0;
+
+        private int lessSign = 0;
+        public ejercicio1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNumeros_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string ingresados = txtNumeros.Text;
+            int pos = (ingresados.Length);
+            // MessageBox.Show(ingresados);
+
+            bool numeroASCII = e.KeyChar >= '0' && e.KeyChar <= '9';
+            bool puntoASCII = e.KeyChar == '.';
+            bool menosASCII = e.KeyChar == '-';
+
+            if (!(numeroASCII || puntoASCII || menosASCII || e.KeyChar == (char)Keys.Back || e.KeyChar == (char)Keys.Enter))
+            {
+                MessageBox.Show("Solo se permiten valores numéricos o de punto flotante", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                e.Handled = true;
+
+                  txtNumeros.SelectionStart = pos;
+
+                return;
+            }
+
+            if (e.KeyChar == 8 && pos >= 0)
+            {
+                if (ingresados[pos - 1] == '-')
+                    this.lessSign--;
+
+                if (ingresados[pos - 1] == '.')
+                    this.dotCounted--;
+            }
+
+            if (menosASCII && pos != 0)
+            {
+                MessageBox.Show("El signo de - solo puede ir al inicio de los datos ingresados", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                // ManejoErrores(pos, ingresados);
+
+                e.Handled = true;
+
+                    txtNumeros.SelectionStart = pos + 1;
+
+                return;
+            }
+
+            if (puntoASCII)
+                this.dotCounted++;
+
+            if (menosASCII)
+                this.lessSign++;
+
+            //if (this.dotCounted == 1 && pos == 0 && puntoASCII)
+            //{
+            //    txtNumeros.Text = " ";
+            //    txtNumeros.Text = "0.";
+            //}
+                
+
+            bool verifyDot = this.dotCounted > 1;
+            bool verifyLessSign = this.lessSign > 1;
+
+            if (verifyDot || verifyLessSign)
+            {
+                string errorUser = verifyDot ? "." : "-";
+                MessageBox.Show($"Solo se permite un {errorUser} para los valores con punto flotante", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                // ManejoErrores(pos, ingresados);
+
+                if (verifyDot)
+                    this.dotCounted--;
+
+                if (verifyLessSign)
+                    this.lessSign--;
+
+                e.Handled = true;
+                return;
+            }
+                    txtNumeros.SelectionStart = pos + 1;
+        }
+
+
+        private void txtNumeros_KeyUp(object sender, KeyEventArgs e)
+        {
+
+            string ingresados = txtNumeros.Text;
+            int pos = (ingresados.Length - 1);
+            bool puntoASCII = e.KeyValue == 190;
+
+            //if (!(numeroASCII || puntoASCII || backSpaceASCII || menosASCII || e.KeyValue == 13))
+            //{
+            //    MessageBox.Show("solo se permite valores números o de punto flotante", "error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            //    e.Handled = true;
+
+            //    txtNumeros.SelectionStart = pos;
+
+            //    return;
+            //}
+
+            //if(menosASCII && pos != 0)
+            //{
+            //    MessageBox.Show("El signo de - solo puede ir al inicio de los datos ingresados", "error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            //    ManejoErrores(pos, ingresados);
+
+            //    e.Handled = true;
+
+            //    txtNumeros.SelectionStart = pos;
+
+            //    return;
+            //}
+
+
+            //if (puntoASCII)
+            //    this.dotCounted++;
+
+            //if (menosASCII)
+            //    this.lessSign++;
+
+            if (this.dotCounted == 1 && pos == 0 && puntoASCII)
+                txtNumeros.Text = "0.";
+                
+
+            //verifyDot = this.dotCounted > 1;
+
+            //verifyLessSign = this.lessSign > 1;
+
+            //if ( verifyDot || verifyLessSign )
+            //{
+
+            //    errorUser = verifyDot ? "." : "-";
+
+            //    MessageBox.Show("solo se permite un "+ errorUser +"para los valores con punto flotante", "error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            //    ManejoErrores(pos, ingresados);
+
+            //    if (verifyDot)
+            //        this.dotCounted--;
+
+            //    if (verifyLessSign)
+            //        this.lessSign--;
+
+            //    e.Handled = true;
+
+            txtNumeros.SelectionStart = 2;
+
+            //    return;
+            //}
+
+        }
+
+        private void txtNumeros_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+        }
+
+        private void ManejoErrores(int pos, string ingresados)
+        {
+            if( pos >= 0 )
+            ingresados = ingresados.Remove(pos, 1).Insert(pos, "");
+
+            txtNumeros.Text = ingresados;
+        }
+    }
+}
